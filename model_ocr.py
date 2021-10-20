1098c4458144d9777b49961ebfb7553d676b74dc
Emerson Rafael <emersona7x@hotmail.com>
2021-10-19 22:07:33 -0300

    [CHG] INCLUSÃO DE PÓS PROCESSAMENTO NOS CAMPOS NUMÉRICOS (RG E CPF) E DATAS (DATA DE EXPEDIÇÃO E DATA DE NASCIMENTO

--- model_ocr.py
+++ model_ocr.py
@@ -92,10 +92,27 @@ class Execute_OCR_RG(object):
         }
 
         # 6 - DEFININDO REGEX
+
+        # SELECIONA APENAS LETRAS
         self.regex_only_letters = r'[^A-zÀ-ù]'
+
+        # SELECIONA APENAS NÚMEROS
         self.regex_only_numbers = r'[^\d]'
-        self.regex_only_dates = r'[0-9]{2}(\/|-)(\w){2}(\w)?(\/|-)[0-9]{4}'
-        self.regex_only_letters_dot_dash = '[^(A-zÀ-ù)(\-)(.)]'
+
+        # SELECIONA APENAS LETRAS E NÚMEROS
+        self.regex_only_letters_numbers = r'[^A-Za-z0-9]+'
+
+        # SELECIONA APENAS LETRAS, PONTOS, BARRAS, TRAÇOS E NÚMEROS
+        self.regex_only_letters_numbers_dot_bars_dashes = r'[^A-Za-z0-9\-.\/]'
+
+        # SELECIONA APENAS A LETRA X, PONTOS, BARRAS, TRAÇOS E NÚMEROS
+        self.regex_only_x_numbers_dot_bars_dashes = r'[^xX0-9\-.\/]'
+
+        # SELECIONA APENAS DATAS
+        self.regex_only_dates = r'[0-9]{1,2}(\/|-)[A-Za-z0-9]{1,3}(\/|-)[0-9]{2,4}'
+
+        # SELECIONA APENAS LETRAS, PONTO (.) E TRAÇO (-)
+        self.regex_only_letters_dot_dash = r'[^(A-zÀ-ù)(\-)(.)]'
 
 
     @property
@@ -127,8 +144,76 @@ class Execute_OCR_RG(object):
 
     def __postprocess_string(self, field):
 
+        """
+
+            APLICA TÉCNICAS DE PÓS PROCESSAMENTO:
+
+                1) MANTÉM APENAS LETRAS
+                2) RETIRA ESPAÇOS EM EXCESSO
+
+            # Arguments
+                field              - Required : Valor a ser pós processado (String)
+
+            # Returns
+                output             - Required : Valor após processamento (String)
+
+        """
+
         try:
+            # MANTENDO APENAS LETRAS
             output = re.sub(self.regex_only_letters, " ", field).replace("  ", " ").strip()
+        except Exception as ex:
+            print("ERRO NA FUNÇÃO {} - {}".format(stack()[0][3], ex))
+            output = field
+
+        return output
+
+
+    def __postprocess_num_rg(self, field):
+
+        """
+
+            APLICA TÉCNICAS DE PÓS PROCESSAMENTO:
+
+                1) MANTÉM APENAS LETRAS, NÚMEROS, PONTOS ('.') BARRAS ('/') E TRAÇOS ('-')
+                2) RETIRA ESPAÇOS EM EXCESSO
+
+            # Arguments
+                field              - Required : Valor a ser pós processado (String)
+
+            # Returns
+                output             - Required : Valor após processamento (String)
+
+        """
+
+        try:
+            # MANTENDO APENAS LETRAS, NÚMEROS, PONTOS ('.') BARRAS ('/') E TRAÇOS ('-')
+            output = re.sub(self.regex_only_x_numbers_dot_bars_dashes, "", field).replace("  ", " ").strip()
+
+            # SUBSTITUINDO '/' POR '-'
+            output = re.sub(r"/", "-", output)
+
+            # VERIFICANDO SE "-" CONSTA NO TEXTO EXTRAIDO PELO OCR
+            if "-" in output:
+                preffix = output.split("-")[0]
+                suffix = "-" + output.split("-")[-1]
+            else:
+                preffix = output
+                suffix = ""
+
+            # APLICANDO REGRAS DE VALIDAÇÃO DO NÚMERO DE RG
+            out_pre = ""
+
+            for i, digit in enumerate(preffix[::-1]):
+                if i%3==0:
+                    out_pre += "."
+                out_pre += digit
+
+            out_pre = out_pre[::-1].strip(".")
+
+            # FORMATANDO O OUTPUT FINAL, USANDO A COMBINAÇÃO DA VALIDAÇÃO E DO SUFFIXO
+            output = out_pre + suffix
+
         except Exception as ex:
             print("ERRO NA FUNÇÃO {} - {}".format(stack()[0][3], ex))
             output = field
@@ -138,22 +223,66 @@ class Execute_OCR_RG(object):
 
     def __postprocess_num(self, field):
 
-        output = re.sub(r"[^\d|\-|\/]", "", field)
-        output = re.sub(r"/", "-", output)
+        """
 
-        if "-" in output:
-            preffix = output.split("-")[0]
-            suffix = "-"+output.split("-")[-1]
-        else:
-            preffix = output
-            suffix = ""
-        out_pre = ""
-        for i, digit in enumerate(preffix[::-1]):
-            if i%3==0:
-                out_pre+="."
-            out_pre+=digit
-        out_pre = out_pre[::-1].strip(".")
-        return out_pre+suffix
+            APLICA TÉCNICAS DE PÓS PROCESSAMENTO:
+
+                1) MANTÉM APENAS LETRAS, NÚMEROS, PONTOS ('.') BARRAS ('/') E TRAÇOS ('-')
+                2) RETIRA ESPAÇOS EM EXCESSO
+
+            # Arguments
+                field              - Required : Valor a ser pós processado (String)
+
+            # Returns
+                output             - Required : Valor após processamento (String)
+
+        """
+
+        try:
+            # MANTENDO APENAS A LETRA X, NÚMEROS, PONTOS ('.') BARRAS ('/') E TRAÇOS ('-')
+            output = re.sub(self.regex_only_x_numbers_dot_bars_dashes, "", field).replace("  ", " ").strip()
+
+            # SUBSTITUINDO '/' POR '-'
+            output = re.sub(r"/", "-", output)
+
+        except Exception as ex:
+            print("ERRO NA FUNÇÃO {} - {}".format(stack()[0][3], ex))
+            output = field
+
+        return output
+
+    def __postprocess_dates(self, field):
+
+        """
+
+            APLICA TÉCNICAS DE PÓS PROCESSAMENTO: PARA CAMPOS DATA
+
+                1) MANTÉM APENAS LETRAS, NÚMEROS, PONTOS ('.') BARRAS ('/') E TRAÇOS ('-')
+                2) RETIRA ESPAÇOS EM EXCESSO
+
+            # Arguments
+                field              - Required : Valor a ser pós processado (String)
+
+            # Returns
+                output             - Required : Valor após processamento (String)
+
+        """
+
+        try:
+            # MANTENDO APENAS LETRAS, NÚMEROS, PONTOS ('.') BARRAS ('/') E TRAÇOS ('-')
+            output = re.sub(self.regex_only_letters_numbers_dot_bars_dashes, "", field).replace("  ", " ").strip()
+
+            # BUSCANDO MATCHS DE DATAS
+            match = re.match(self.regex_only_dates, output)
+
+            if match:
+                output = match[0]
+
+        except Exception as ex:
+            print("ERRO NA FUNÇÃO {} - {}".format(stack()[0][3], ex))
+            output = field
+
+        return output
 
 
     def __postprocess_location(self, field):
@@ -213,11 +342,10 @@ class Execute_OCR_RG(object):
             info_extracted[field] = self.__tesseract.image_to_string(roi).strip()
 
             # VISUALIZANDO O BOUNDING BOX
-            image_view_functions.view_image_with_coordinates(image_view_functions.create_bounding_box(img,
-                                                                                                      bounding_positions))
+            # image_view_functions.view_image_with_coordinates(image_view_functions.create_bounding_box(img, bounding_positions))
 
             # VISUALIZANDO O CROP
-            image_view_functions.view_image_with_coordinates(roi)
+            # image_view_functions.view_image_with_coordinates(roi)
 
         return info_extracted
 
@@ -251,6 +379,8 @@ class Execute_OCR_RG(object):
             info_extracted[column] = self.__postprocess_num(info_extracted[column])
 
         # APLICANDO PÓS PROCESSAMENTO NOS CAMPOS DATAS
+        for column in ["DATA_EXPED", "DATA_NASC"]:
+            info_extracted[column] = self.__postprocess_dates(info_extracted[column])
 
 
         # APLICANDO PÓS PROCESSAMENTO NOS CAMPOS CIDADE E ESTADO ORIGEM
