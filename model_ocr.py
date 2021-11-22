13849725b44e87e2cd4b9b39c0ebc54acbe4f1fc
Emerson Rafael <emersona7x@hotmail.com>
2021-11-22 18:37:31 -0300

    [CHG] ATUALIZAÇÃO DO PÓS PROCESSAMENTO DE LOCALIZAÇÃO (OBTENDO O ESTADO E A CIDADE DE MAIOR SIMILARIDADE. ATUALIZAÇÃO DO PÓS PROCESSAMENTO DOS CAMPOS NUMÉRICOS (RG E CPF) MANTENDO APENAS A LETRA 'X' E NÚMEROS

--- model_ocr.py
+++ model_ocr.py
@@ -36,7 +36,7 @@ import cv2
 from dynaconf import settings
 
 from UTILS.check_similarity import Check_Similarity
-from UTILS.generic_functions import format_values_int
+from UTILS.generic_functions import format_values_int, drop_duplicates_list
 from UTILS.image_view import image_view_functions
 from UTILS.image_ocr import ocr_functions
 from UTILS.conectores_db.main import conectores
@@ -61,7 +61,7 @@ class Execute_OCR_RG(object):
         self.COORDS = self.__get_coords()
 
         # 5 - DEFININDO DE-PARA DE ESTADO-CIDADE
-        self.UF_TO_STATE = self.__get_uf_state()
+        self.CITY_STATE = self.__get_uf_state()
 
         # 6 - DEFININDO REGEX
 
@@ -80,11 +80,23 @@ class Execute_OCR_RG(object):
         # SELECIONA APENAS A LETRA X, PONTOS, BARRAS, TRAÇOS E NÚMEROS
         self.regex_only_x_numbers_dot_bars_dashes = settings.REGEX_ONLY_X_NUMBERS_DOT_BARS_DASHES
 
+        # SELECIONA APENAS A LETRA X E NÚMEROS
+        self.regex_only_x_numbers = settings.REGEX_ONLY_X_NUMBERS
+
         # SELECIONA APENAS DATAS
         self.regex_only_dates = settings.REGEX_ONLY_DATES
 
         # SELECIONA APENAS LETRAS, PONTO (.) E TRAÇO (-)
         self.regex_only_letters_dot_dash = settings.REGEX_ONLY_LETTERS_DOT_DASH
+
+        # 7 - INICIANDO OS PERCENTUAIS DE MATCH DEFAULT
+        self.default_percent_match = settings.DEFAULT_PERCENTUAL_MATCH
+
+        # 8 - DEFININDO SE DEVE HAVER PRÉ PROCESSAMENTO DOS ITENS ANTES DO CÁLCULO DE SEMELHANÇA
+        self.similarity_pre_processing = settings.DEFAULT_PRE_PROCESSING
+
+        # 9 - INICIANDO A VARIÁVEL QUE CONTÉM O LIMIT NA CHAMADA DE MÁXIMAS SIMILARIDADES
+        self.limit_result_best_similar = settings.DEFAULT_LIMIT_RESULT_BEST_SIMILAR
 
 
     @property
@@ -388,8 +400,8 @@ class Execute_OCR_RG(object):
             # SUBSTITUINDO ',' POR '.'
             output = output.replace(",", ".")
 
-            # MANTENDO APENAS A LETRA X, NÚMEROS, PONTOS ('.') BARRAS ('/') E TRAÇOS ('-')
-            output = re.sub(self.regex_only_x_numbers_dot_bars_dashes, "", field).replace("  ", " ").strip()
+            # MANTENDO APENAS A LETRA X E NÚMEROS
+            output = re.sub(self.regex_only_x_numbers, "", field).replace("  ", " ").strip()
 
         except Exception as ex:
             print("ERRO NA FUNÇÃO {} - {}".format(stack()[0][3], ex))
@@ -432,7 +444,160 @@ class Execute_OCR_RG(object):
         return output
 
 
+    @staticmethod
+    def get_best_distance_levenshtein_ocr(list_values):
+
+        # INICIANDO A VARIÁVEL AUXILIAR QUE ARMAZENARÁ A VARIÁVEL COM A
+        # MENOR QUANTIDADE DE DIFERENÇA DE QUANTIDADE ENTRE AS STRINGS
+        min_dif_len_letters = 100
+        result_max_similarity = []
+
+        for value in list_values:
+
+            # CALCULANDO A DIFERENÇA DE QUANTIDADE DE LETRAS
+            dif_len_string = abs(len(value[0]) - len(value[1][0]))
+
+            if dif_len_string < min_dif_len_letters:
+
+                # ARMAZENANDO O RESULTADO
+                result_max_similarity = value
+
+                # ATUALIZANDO O VALOR DE MÍNIMA DIFERENÇA DE TAMANHO DA STRING
+                min_dif_len_letters = dif_len_string
+
+        # RETORNANDO O VALOR DE MÁXIMA SIMILARIDADE
+        return result_max_similarity
+
+
+    def get_max_similarity(self, text, list_values_similitary):
+
+        """
+
+            RECEBE UMA STRING, SEPARA-SE OS VALORES POR " " E "-".
+
+                1) SPLIT POR " " E "-"
+                2) CADA UM DOS VALORES É ENVIADO PARA OBTER A MÁXIMA SIMILARIDADE
+                3) RETORNA O VALOR DE MAIOR SIMILARIDADE
+
+            # Arguments
+                text                       - Required : Texto a ser processado (String)
+                list_values_similitary     - Required : Lista de valores possíveis (List)
+
+            # Returns
+                result_max_similarity      - Required : Valor de máxima similaridade com o texto (String)
+
+        """
+
+        # INICIANDO A VARIÁVEL AUXILIAR QUE ARMAZENARÁ O MAX MATCH DE SIMILARIDADE
+        max_similarity = 0
+        result_max_similarity = []
+
+        # REALIZANDO A QUEBRA POR ESPAÇOS E - E PERCORRENDO CADA UM DOS VALORES
+        for value_x in text.split(" "):
+
+            for value_j in value_x.split("-"):
+
+                if value_j != "":
+
+                    # OBTENDO A CIDADE E O ESTADO CONTIDO NO TEXTO
+                    result_similarity = Execute_OCR_RG.get_similitary(self,
+                                                                      value_j,
+                                                                      list_values_similitary,
+                                                                      self.default_percent_match,
+                                                                      self.similarity_pre_processing,
+                                                                      self.limit_result_best_similar)
+
+                    if result_similarity[0]:
+
+                        if result_similarity[1][0][1] >= max_similarity:
+
+                            # ARMAZENANDO O RESULTADO
+                            result_max_similarity.append([value_j, result_similarity[1][0]])
+
+                            # ATUALIZANDO O VALOR DE MÁXIMA SIMILARIDADE
+                            max_similarity = result_max_similarity[-1][1][1]
+
+                            if max_similarity == 100:
+                                # RETORNANDO O VALOR DE MÁXIMA SIMILARIDADE
+                                return result_max_similarity[-1][1]
+
+        # RETORNANDO O VALOR DE MÁXIMA SIMILARIDADE
+        return Execute_OCR_RG.get_best_distance_levenshtein_ocr(result_max_similarity)[-1]
+
+
     def __postprocess_location(self, field):
+
+        """
+
+            APLICA TÉCNICAS DE PÓS PROCESSAMENTO: PARA CAMPOS CIDADE - ESTADO
+
+                1) MANTÉM APENAS LETRAS, PONTOS ('.') E TRAÇOS ('-')
+                2) RETIRA ESPAÇOS EM EXCESSO
+
+            NORMALMENTE CIDADE E ESTADO VEM PRÓXIMOS UM AO OUTRO
+            EX: CIDADE-UF
+
+            NESSE CASO, SEPARA-SE OS VALORES POR " " E "-".
+
+                3) SPLIT POR " " E "-"
+                4) CADA UM DOS VALORES É ENVIADO PARA OBTER A MÁXIMA SIMILARIDADE
+                5) PRIMEIRO OBTÉM-SE O UF COM MAIOR SIMILARIDADE
+                6) FILTRAM-SE AS CIDADES REFERENTES A ESSE UF
+                7) OBTÉM-SE A CIDADE COM MAIOR SIMILARIDADE
+
+            # Arguments
+                field              - Required : Valor a ser pós processado (String)
+
+            # Returns
+                city               - Required : Cidade após processamento (String)
+                state              - Required : Estado após processamento (String)
+
+        """
+
+        # INICIANDO AS VARIÁVEIS RESULTANTES DE CIDADE E ESTADO
+        city = state = ""
+
+        # INICIANDO A VARIÁVEL AUXILIAR QUE ARMAZENARÁ O MAX MATCH DE SIMILARIDADE
+        max_similarity = 0
+        result_max_similarity = []
+
+        field = re.sub(r",", ".", field)
+        field = re.sub(r"=", "-", field)
+
+        # TRATANDO O VALOR DO CAMPO
+        field = re.sub(self.regex_only_letters_dot_dash, " ", field).replace("  ", " ").strip()
+
+        # OBTENDO ESTADO - CIDADE
+        for current_field in ["state", "city"]:
+
+            if current_field == "state":
+                # OBTENDO OS VALORES DE UF PARA COMPARAR
+                list_values_similitary = drop_duplicates_list([value[1] for value in self.CITY_STATE])
+
+            else:
+                # OBTENDO OS VALORES DE CIDADES PARA COMPARAR
+                # VERIFICANDO SE HÁ UM ESTADO DEFINIDO
+                if state != "":
+                    list_values_similitary = drop_duplicates_list([value[0] for value in self.CITY_STATE if value[1] == state])
+                else:
+                    list_values_similitary = drop_duplicates_list([value[0] for value in self.CITY_STATE])
+
+            # OBTENDO O VALOR DE MÁXIMA SIMILARIDADE
+            result_max_similarity = Execute_OCR_RG.get_max_similarity(self, field, list_values_similitary)
+
+            if current_field == "state":
+                # OBTENDO O VALOR RESULTANTE
+                state = result_max_similarity[0]
+
+            else:
+                # OBTENDO O VALOR RESULTANTE
+                city = result_max_similarity[0]
+
+        # RETORNANDO OS VALORES DE ESTADO E CIDADE
+        return city, state
+
+
+    def __postprocess_location_old(self, field):
 
         # INICIANDO AS VARIÁVEIS RESULTANTES DE CIDADE E ESTADO
         city = state = ""
@@ -443,8 +608,18 @@ class Execute_OCR_RG(object):
         # TRATANDO O VALOR DO CAMPO
         field = re.sub(self.regex_only_letters_dot_dash, " ", field).replace("  ", " ").strip()
 
-        # OBTENDO A CIDADE E O ESTADO CONTIDO NO TEXTO
+        for value in field.split(" "):
 
+            # OBTENDO A CIDADE E O ESTADO CONTIDO NO TEXTO
+            result_similarity = Execute_OCR_RG.get_similitary(self,
+                                                              value,
+                                                              [value[0] for value in self.CITY_STATE],
+                                                              self.default_percent_match,
+                                                              self.similarity_pre_processing,
+                                                              self.limit_result_best_similar)
+
+            if result_similarity[0]:
+                print(result_similarity)
 
         try:
             result_split = field.split("-")
@@ -458,7 +633,7 @@ class Execute_OCR_RG(object):
             city = re.sub(self.regex_only_letters, " ", city).replace("  ", " ").strip()
 
             # OBTENDO O ESTADO\
-            list_result_state = [key for key in self.UF_TO_STATE if (self.UF_TO_STATE[key] == city)]
+            list_result_state = [key for key in self.CITY_STATE if (self.CITY_STATE[key] == city)]
             if len(list_result_state) > 0:
                 state = list_result_state[0]
 
@@ -554,8 +729,12 @@ class Execute_OCR_RG(object):
             info_extracted[column] = self.__postprocess_dates(info_extracted[column])
 
         # APLICANDO PÓS PROCESSAMENTO NOS CAMPOS CIDADE E ESTADO ORIGEM
-        info_extracted["CIDADE_ORIGEM"], info_extracted["UF_ORIGEM"] = self.__postprocess_location(
+        info_extracted["CIDADE_ORIGEM"], info_extracted["ESTADO_ORIGEM"] = self.__postprocess_location(
             info_extracted["CIDADE_ORIGEM"])
+
+        # APLICANDO PÓS PROCESSAMENTO NOS CAMPOS CIDADE E ESTADO NASCIMENTO
+        info_extracted["CIDADE_NASC"], info_extracted["ESTADO_NASC"] = self.__postprocess_location(
+            info_extracted["CIDADE_NASC"])
 
         # RETORNANDO OS DADOS APÓS APLICAÇÃO DOS PÓS PROCESSAMENTOS
         return info_extracted
