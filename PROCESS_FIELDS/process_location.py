f3e1c1430020132f860b4d6d9d97646bb399c8d3
Emerson Rafael <emersona7x@hotmail.com>
2021-11-24 03:46:21 -0300

    [ADD] CRIAÇÃO DAS PIPELINES DE PROCESSAMENTO EM ARQUIVOS SEPARADOS (LOCALIZAÇÃO E NOMES)

---
+++ PROCESS_FIELDS/process_location.py
@@ -0,0 +1,231 @@
+from inspect import stack
+
+from dynaconf import settings
+import re
+
+from UTILS.conectores_db.main import conectores
+from UTILS.extract_infos import Extract_Infos
+from UTILS.generic_functions import drop_duplicates_list, read_txt
+
+
+class Execute_Process_Location():
+
+    def __init__(self):
+
+        # 1 - DEFININDO DE-PARA DE ESTADO-CIDADE
+        self.CITY_STATE = self.__get_uf_state()
+
+        # 2 - INICIANDO OS PERCENTUAIS DE MATCH DEFAULT
+        self.default_percent_match = settings.DEFAULT_PERCENTUAL_MATCH
+
+        # 3 - DEFININDO SE DEVE HAVER PRÉ PROCESSAMENTO DOS ITENS ANTES DO CÁLCULO DE SEMELHANÇA
+        self.similarity_pre_processing = settings.DEFAULT_PRE_PROCESSING
+
+        # 4 - INICIANDO A VARIÁVEL QUE CONTÉM O LIMIT NA CHAMADA DE MÁXIMAS SIMILARIDADES
+        self.limit_result_best_similar = settings.DEFAULT_LIMIT_RESULT_BEST_SIMILAR
+
+        self.regex_only_letters_dot_dash = Extract_Infos().get_value_regex("REGEX_ONLY_LETTERS_DOT_DASH")
+
+        # 5 - INSTANCIANDO O OBJETO DA CLASSE DE EXTRAÇÃO DE INFOS
+        self.orchestra_extract_infos = Extract_Infos()
+
+
+    def __get_uf_state(self):
+
+        """
+
+            OBTÉM TODAS OS MUNICIPIOS/ESTADOS DISPONÍVEIS NA BASE DO IBGE.
+
+            # Arguments
+
+            # Returns
+                df_municipios_uf             - Required : Lista contendo municipios/estados (List)
+
+        """
+
+        # INICIANDO A VARIÁVEL RESULTANTE
+        df_municipios_uf = []
+
+        try:
+            # DEFININDO OS PARÂMETROS DE CONEXÃO
+            caminho_bd_bds = settings.DIR_BD_OCR
+            ssql_bds = settings.QUERY_MUNICIPIOS_UF
+            params_bds = (None,)
+            tipo_query_bds = settings.QUERY_TYPE_MUNICIPIOS_UF
+
+            # EXECUTANDO A QUERY E OBTENDO O RESULTADO
+            df_municipios_uf = conectores().execute_query_sqlite(caminho_bd_bds, ssql_bds, params_bds, tipo_query_bds)[1]
+
+        except Exception as ex:
+            print("ERRO NA FUNÇÃO {} - {}".format(stack()[0][3], ex))
+
+        # RETORNANDO A TUPLA CONTENDO (MUNICIPIO, UF, ESTADO)
+        return df_municipios_uf
+
+
+    def get_uf(self, field):
+
+        """
+
+            APLICA TÉCNICAS DE PÓS PROCESSAMENTO: PARA CAMPOS ESTADO
+
+                1) MANTÉM APENAS LETRAS, PONTOS ('.') E TRAÇOS ('-')
+                2) RETIRA ESPAÇOS EM EXCESSO
+                3) OBTEM A MÁXIMA SIMILARIDADE
+
+
+            # Arguments
+                field              - Required : Valor a ser pós processado (String)
+
+            # Returns
+                state              - Required : Estado após processamento (String)
+
+        """
+
+        # INICIANDO AS VARIÁVEIS RESULTANTES DE CIDADE E ESTADO
+        state = ""
+
+        # INICIANDO A VARIÁVEL AUXILIAR QUE ARMAZENARÁ O MAX MATCH DE SIMILARIDADE
+        max_similarity = 0
+        result_max_similarity = []
+
+        field = re.sub(r",", ".", field)
+        field = re.sub(r"=", "-", field)
+
+        # TRATANDO O VALOR DO CAMPO
+        field = re.sub(self.regex_only_letters_dot_dash, " ", field).replace("  ", " ").strip()
+
+        list_values_similitary = drop_duplicates_list([value[1] for value in self.CITY_STATE])
+
+        # OBTENDO O VALOR DE SIMILARIDADE
+        result_similarity = Extract_Infos().get_similitary(field,
+                                                           list_values_similitary,
+                                                           self.default_percent_match,
+                                                           self.similarity_pre_processing,
+                                                           self.limit_result_best_similar)
+
+        # RETORNANDO OS VALORES SIMILARIDADE PARA O ESTADO
+        return result_similarity
+
+
+    def orchestra_postprocess_location(self, field):
+
+        """
+
+            APLICA TÉCNICAS DE PÓS PROCESSAMENTO: PARA CAMPOS CIDADE - ESTADO
+
+                1) MANTÉM APENAS LETRAS, PONTOS ('.') E TRAÇOS ('-')
+                2) RETIRA ESPAÇOS EM EXCESSO
+
+            NORMALMENTE CIDADE E ESTADO VEM PRÓXIMOS UM AO OUTRO
+            EX: CIDADE-UF
+
+            NESSE CASO, SEPARA-SE OS VALORES POR " " E "-".
+
+                3) SPLIT POR " " E "-"
+                4) CADA UM DOS VALORES É ENVIADO PARA OBTER A MÁXIMA SIMILARIDADE
+                5) PRIMEIRO OBTÉM-SE O UF COM MAIOR SIMILARIDADE
+                6) FILTRAM-SE AS CIDADES REFERENTES A ESSE UF
+                7) OBTÉM-SE A CIDADE COM MAIOR SIMILARIDADE
+
+            # Arguments
+                field              - Required : Valor a ser pós processado (String)
+
+            # Returns
+                city               - Required : Cidade após processamento (String)
+                state              - Required : Estado após processamento (String)
+
+        """
+
+        # INICIANDO AS VARIÁVEIS RESULTANTES DE CIDADE E ESTADO
+        city = state = ""
+
+        # INICIANDO A VARIÁVEL AUXILIAR QUE ARMAZENARÁ O MAX MATCH DE SIMILARIDADE
+        max_similarity = 0
+        result_max_similarity = []
+
+        field = re.sub(r",", ".", field)
+        field = re.sub(r"=", "-", field)
+
+        # TRATANDO O VALOR DO CAMPO
+        field = re.sub(self.regex_only_letters_dot_dash, " ", field).replace("  ", " ").strip()
+
+        # OBTENDO ESTADO - CIDADE
+        for current_field in ["state", "city"]:
+
+            if current_field == "state":
+                # OBTENDO OS VALORES DE UF PARA COMPARAR
+                list_values_similitary = drop_duplicates_list([value[1] for value in self.CITY_STATE])
+
+            else:
+                # OBTENDO OS VALORES DE CIDADES PARA COMPARAR
+                # VERIFICANDO SE HÁ UM ESTADO DEFINIDO
+                if state != "":
+                    list_values_similitary = drop_duplicates_list([value[0] for value in self.CITY_STATE if value[1] == state])
+                else:
+                    list_values_similitary = drop_duplicates_list([value[0] for value in self.CITY_STATE])
+
+            # OBTENDO O VALOR DE MÁXIMA SIMILARIDADE
+            result_max_similarity = self.orchestra_extract_infos.get_max_similarity(self, field, list_values_similitary)
+
+            if current_field == "state":
+                # OBTENDO O VALOR RESULTANTE
+                state = result_max_similarity[0]
+
+            else:
+                # OBTENDO O VALOR RESULTANTE
+                city = result_max_similarity[0]
+
+        # RETORNANDO OS VALORES DE ESTADO E CIDADE
+        return city, state
+
+
+    def __postprocess_location_old(self, field):
+
+        # INICIANDO AS VARIÁVEIS RESULTANTES DE CIDADE E ESTADO
+        city = state = ""
+
+        field = re.sub(r",", ".", field)
+        field = re.sub(r"=", "-", field)
+
+        # TRATANDO O VALOR DO CAMPO
+        field = re.sub(self.regex_only_letters_dot_dash, " ", field).replace("  ", " ").strip()
+
+        for value in field.split(" "):
+
+            # OBTENDO A CIDADE E O ESTADO CONTIDO NO TEXTO
+            result_similarity = self.orchestra_extract_infos.get_similitary(value,
+                                                                            [value[0] for value in self.CITY_STATE],
+                                                                            self.default_percent_match,
+                                                                            self.similarity_pre_processing,
+                                                                            self.limit_result_best_similar)
+
+            if result_similarity[0]:
+                print(result_similarity)
+
+        try:
+            result_split = field.split("-")
+
+            if len(result_split) == 1:
+                city = result_split[0]
+            elif len(result_split) >= 2:
+                city, state = result_split[0], result_split[1]
+
+            # TRATANDO O VALOR DA CIDADE
+            city = re.sub(self.regex_only_letters, " ", city).replace("  ", " ").strip()
+
+            # OBTENDO O ESTADO\
+            list_result_state = [key for key in self.CITY_STATE if (self.CITY_STATE[key] == city)]
+            if len(list_result_state) > 0:
+                state = list_result_state[0]
+
+            # RETIRANDO ESPAÇOS ANTES E APÓS OS VALORES OBTIDOS
+            city = city.strip()
+            state = state.strip()
+
+        except Exception as ex:
+            print("ERRO NA FUNÇÃO {} - {}".format(stack()[0][3], ex))
+            city = field
+            state = ''
+
+        return city, state
---
+++ PROCESS_FIELDS/process_names.py
@@ -0,0 +1,244 @@
+from inspect import stack
+
+from dynaconf import settings
+
+from UTILS.extract_infos import Extract_Infos
+from UTILS.generic_functions import read_txt
+
+
+class Execute_Process_Names():
+
+    def __init__(self):
+
+        # 1 - REALIZANDO A LEITURA DO BANCO DE DADOS DE NOMES - GENEROS
+        self.data_first_names_gender = self.__get_data_names(settings.DIR_DATA_FIRST_NAMES_GENDER,
+                                                             value_split=",")
+
+        # 2 - REALIZANDO A LEITURA DO BANCO DE DADOS DE SOBRENOMES
+        self.data_last_names = self.__get_data_names(settings.DIR_DATA_LAST_NAMES,
+                                                     value_split="-")
+
+        # 3 - INICIANDO OS PERCENTUAIS DE MATCH DEFAULT
+        self.default_percent_match = settings.DEFAULT_PERCENTUAL_MATCH
+
+        # 4 - DEFININDO SE DEVE HAVER PRÉ PROCESSAMENTO DOS ITENS ANTES DO CÁLCULO DE SEMELHANÇA
+        self.similarity_pre_processing = settings.DEFAULT_PRE_PROCESSING
+
+        # 5 - INICIANDO A VARIÁVEL QUE CONTÉM O LIMIT NA CHAMADA DE MÁXIMAS SIMILARIDADES
+        self.limit_result_best_similar = settings.DEFAULT_LIMIT_RESULT_BEST_SIMILAR
+
+
+    def __get_data_names(self, dir_data, value_split):
+
+        """
+
+            1) REALIZA A LEITURA DO BANCO DE DADOS DE NOMES
+            2) REALIZA A FORMAAÇÃO DA LISTA, DE ACORDO COM UM VALOR DE SPLIT DEFINIDO
+
+            # Arguments
+                dir_data              - Required : Diretório da base a ser lida (String)
+                value_split           - Required : Caracter utilizada para split da lista (String)
+
+            # Returns
+                list_result          - Required : Lista resultado (List)
+
+        """
+
+        # INICIANDO A VARIÁVEL RESULTANTE
+        list_result = []
+
+        try:
+            # REALIZANDO A LEITURA DO TXT CONTENDO OS DADOS
+            validador, data = read_txt(dir_data)
+
+            if validador:
+
+                # REALIZANDO A CONVERSÃO
+                list_result = [name_sex.split(value_split) for name_sex in data.split("\n")]
+
+        except Exception as ex:
+            print("ERRO NA FUNÇÃO {} - {}".format(stack()[0][3], ex))
+
+        # RETORNANDO A TUPLA CONTENDO O RESULTADO FINAL APÓS LEITURA E FORMATAÇÃO
+        return list_result
+
+
+    def get_first_name_valid(self, name_input, list_first_names=None):
+
+        """
+
+            1) RECEBE UMA STRING CONTENDO UM NOME COMPLTO
+            2) RECEBE UMA LISTA DE NOMES
+            3) VALIDA SE DENTRO OS VALORES DA STRING,
+            HÁ ALGUM VÁLIDO COMO PRIMEIRO NOME.
+
+            # Arguments
+                name_input         - Required : Nome completo a ser analisado (String)
+                list_first_names   - Optional : Lista de primeiros nomes e generos (List)
+
+            # Returns
+                first_name         - Required : Primeiro nome válido (String)
+                gender             - Required : Gênero do nome validado (Char)
+
+        """
+
+        # INICIANDO A VARIÁVEL QUE O PRIMEIRO NOME VÁLIDO
+        first_name = ""
+        gender = ""
+
+        # VERIFICANDO A  VARIÁVEL DE PRIMEIROS NOMES
+        if list_first_names is None:
+            list_first_names = self.data_first_names_gender
+
+        for value_x in name_input.split(" "):
+
+            if value_x != "" and len(value_x) >= 2:
+
+                result_similarity = Extract_Infos.get_similitary(self,
+                                                                 value_x,
+                                                                 [value[0] for value in list_first_names],
+                                                                 self.default_percent_match,
+                                                                 self.similarity_pre_processing,
+                                                                 self.limit_result_best_similar)
+
+                if result_similarity[0]:
+
+                    # VERIFICANDO SE O MATCH OCORREU PARA LEN(NOME) < 4, COM VALOR DE 100
+                    # CASO NÃO TENHA SIDO, EXCLUI A OPÇÃO
+                    if result_similarity[-1][0][-1] < 100 and len(value_x) <= 3:
+                        # CASO RECUSADO, CONTINUA BUSCANDO UM NOME VÁLIDO
+                        continue
+                    else:
+                        print(result_similarity[-1])
+
+                        # ARMAZENANDO O NOME SALVO
+                        first_name = value_x
+
+                        try:
+                            # OBTENDO O GÊNERO
+                            gender = [value[1].upper() for value in list_first_names if
+                                      value[0].upper() == result_similarity[-1][0][0].upper()][0]
+                        except Exception as ex:
+                            # OBTENDO O GÊNERO
+                            gender = [value[1].upper() for value in self.data_first_names_gender if
+                                      value[0].upper() == result_similarity[-1][0][0][0].upper()][0]
+
+                        return result_similarity, first_name, gender
+
+        return result_similarity, first_name, gender
+
+
+    def last_name_valid(self, complete_name_input, first_name_input, list_last_names=None):
+
+        """
+
+            1) RECEBE UMA STRING CONTENDO UM NOME COMPLTO
+            2) RECEBE UMA LISTA DE SOBRENOMES
+            3) VALIDA CADA UM DOS VALORES DO NOME,
+            SE ESSE VALOR É UM SOBRENOME
+
+            # Arguments
+                complete_name_input  - Required : Nome completo a ser analisado (String)
+                first_name_input     - Required : Primeiro nome validado (String)
+                list_last_names      - Optional : Lista de sobrenomes (List)
+
+            # Returns
+                last_name          - Required : Sobrenomes válidos (String)
+                gender             - Required : Gênero do nome validado (Char)
+
+        """
+
+        # INICIANDO A VARIÁVEL QUE OS SOBRENOMES VÁLIDOS
+        last_name = ""
+
+        # VERIFICANDO A  VARIÁVEL DE PRIMEIROS NOMES
+        if list_last_names is None:
+            list_last_names = self.data_last_names
+
+        for value_x in complete_name_input.split(" "):
+
+            # VALIDANDO SE É O PRIMEIRO NOME (ESSE NÃO SERÁ ANALISADO)
+            if value_x != first_name_input and value_x not in ["DO", "DA"]:
+
+                # VALIDANDO SE É O PRIMEIRO NOME (ESSE NÃO SERÁ ANALISADO)
+                if value_x != "" and len(value_x) >= 3:
+
+                    result_similarity = Extract_Infos.get_similitary(self,
+                                                                     value_x,
+                                                                     [value[0] for value in list_last_names],
+                                                                     self.default_percent_match,
+                                                                     self.similarity_pre_processing,
+                                                                     self.limit_result_best_similar)
+
+                    if result_similarity[0]:
+                        print(value_x, result_similarity[-1])
+
+                        # ARMAZENANDO O NOME SALVO
+                        last_name += value_x + " "
+
+            else:
+                # ARMAZENANDO O NOME SALVO
+                last_name += value_x + " "
+
+        return last_name.strip()
+
+
+    def orchestra_postprocess_names(self, info_extracted):
+
+        """
+
+            APLICA TÉCNICAS DE PÓS PROCESSAMENTO:
+
+                1) RETIRA VALORES QUE NÃO SÃO NOME E/OU SOBRENOMES.
+
+            # Arguments
+                field              - Required : Valor a ser pós processado (String)
+
+            # Returns
+                output             - Required : Valor após processamento (String)
+
+        """
+
+        dict_names = ["NOME", "NOME_MAE", "NOME_PAI"]
+
+        for field in dict_names:
+
+            # FILTRANDO O VALOR DO CAMPO ATUAL
+            name_input = info_extracted[field]
+
+            if " E " in name_input and field == "NOME_MAE":
+
+                result_split = name_input.split(" E ")
+
+                # ATUALIZANDO O VALOR DO CAMPO NOME_MAE
+                name_input = result_split[-1]
+
+                # INSERINDO O VALOR ANTES DO E NO CAMPO NOME_PAI
+                if name_input.split(" E ")[0] not in dict_names["NOME_PAI"]:
+                    dict_names["NOME_PAI"] += " " + result_split[0]
+
+            print("CAMPO ATUAL: {}".format(field))
+            print("VALOR DE INPUT: {}".format(name_input))
+
+            # OBTENDO O PRIMEIRO NOME VÁLIDO
+            result_similarity, first_name, gender = Execute_Process_Names.get_first_name_valid(self, name_input,
+                                                                                               self.data_first_names_gender)
+
+            # FILTRANDO O NOME CONFORME O PRIMEIRO NOME VÁLIDO
+            result_first_name = name_input[name_input.find(first_name):]
+
+            print(result_first_name)
+            print("GÊNERO: {}".format(gender))
+
+            # VALIDANDO OS SOBRENOMES
+            result_last_name = Execute_Process_Names.last_name_valid(self, result_first_name,
+                                                                     first_name,
+                                                                     self.data_last_names)
+            print(result_last_name)
+
+            print("-" * 50)
+
+            # ATUALIZADO O NOME
+            info_extracted[field] = result_last_name
+
+        return info_extracted
